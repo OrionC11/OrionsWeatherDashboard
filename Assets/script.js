13e01d2bcdd17cc8e584885ca091eda700a73ac3
Orion Cannon <orion.m.cannon@gmail.com>
2024-05-01 19:16:05 -0700

    added the current weather api call and a function to display the data

--- Assets/script.js
+++ Assets/script.js
@@ -28,3 +28,35 @@ function cityGeo(city) {
     });
 }
 
+function getWeather(lat, lon) {
+  var weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
+  fetch(weatherAPI)
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log("Weather Data", data);
+      displayWeather(data);
+    });
+}
+
+
+function displayWeather(data) {
+  var cityWeather = document.createElement("div");
+  cityWeather.innerHTML = `<div class="today">
+  <div class="todayDirection">
+  <h2>${data.name}</h2>
+  <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="weather icon" />
+  </div>
+  <h3>Current Weather</h3>
+  <ul class="todayDetails">
+  <li> Temperature: ${data.main.temp}°F</li>
+  <li> Humidity: ${data.main.humidity}%</li>
+  <li> Wind Speed: ${data.wind.speed} MPH</li>
+  <li> Description: ${data.weather[0].description}</li>
+  
+  </ul>
+  </div>`;
+  currentWeather.append(cityWeather);
+}
+
+
+searchBtn.addEventListener("click", search);
