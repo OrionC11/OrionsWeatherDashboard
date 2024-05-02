9d8274d3216e6fe1974c1b9fc147230751811dc7
Orion Cannon <orion.m.cannon@gmail.com>
2024-05-01 19:14:55 -0700

    added the first api call

--- Assets/script.js
+++ Assets/script.js
@@ -15,3 +15,16 @@ function search(event) {
   cityGeo(city);
 }
 
+function cityGeo(city) {
+  var geoAPI = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+  fetch(geoAPI)
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log("Geo Data", data);
+      console.log("data lat:", data.coord.lat);
+      console.log("data lon:", data.coord.lon);
+      getWeather(data.coord.lat, data.coord.lon);
+      get5Day(data.coord.lat, data.coord.lon);
+    });
+}
+
